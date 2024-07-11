110c5cde92aaf83764a77dd6035b1f38b20fffb3
MaximoColombatto4ac <maximo.colombatto@gmail.com>
2024-07-11 17:30:10 -0300

    making front for auth

---
+++ server/src/controllers/auth.controllers.ts
@@ -0,0 +1,33 @@
+import bcrypt from "bcrypt";
+import {IUsuario} from "../interfaces/Usuario"
+import { Usuario } from "../models/auth.models";
+import { Request as IReq, Response as IRes } from 'express';
+import { createAccesToken } from "../libs/jwt";
+
+export const login = async (req: IReq, res: IRes) => {
+    const {nombre, contraseña}: IUsuario = req.body;
+
+    const found = await Usuario.findOne({nombre});
+    if (!found) return res.status(400).json(["usuario no encontrado"]); 
+
+    const match = await bcrypt.compare(contraseña, found.contraseña)
+    if(!match) return res.status(500).json(["password incorrect"]);
+    
+    const token = await createAccesToken({_id: found._id,nombre: found.nombre, email: found.email});
+    
+    res.json({ message: 'Login successful', token });
+}
+
+export const register = async (req: IReq, res: IRes) => {
+    const {nombre, email, contraseña}: IUsuario = req.body;
+    const found = await Usuario.findOne({nombre, email});
+    if (found) return res.status(400).json(["nombre/email en uso"]);
+
+    const hash = await bcrypt.hash(contraseña, 10);
+
+    const log = await new Usuario({nombre, email, contraseña:hash}).save();
+    
+    const token = await createAccesToken({_id: log._id,nombre: log.nombre, email: log.email});
+    
+    res.status(201).json({ message: 'Login successful', token });
+}
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,6 @@
 import validate from "../middleware/validateSchema"
 import { Router } from 'express'
-import { login, register, logout } from '../controllers/auth.controllers'
+import { login, register } from '../controllers/auth.controllers'
 import { loginSchema, registerSchema } from '../schema/auth.schema'
 
 
@@ -8,6 +8,5 @@ const auth = Router()
 
 auth.post('/login', validate(loginSchema), login)
 auth.post('/register', validate(registerSchema), register)
-auth.get('/logout', logout)
 
 export default auth;
