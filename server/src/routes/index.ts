1d61c9c6f07ec29381966241fba2174744c14af7
maxi <maximo.colombatto@gmail.com>
2024-07-24 13:34:41 -0300

    solve headear issues

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import auth from "./auth";
+import user from "./user";
 
 const router = Router();
 
 router.use('/api',auth);
+router.use('/user', user);
 
 export default router;
