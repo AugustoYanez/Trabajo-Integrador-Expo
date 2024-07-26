1c30878bc346ff42370a4c8c733ceba8a4f5ee49
maxi <maximo.colombatto@gmail.com>
2024-07-25 23:07:43 -0300

    ll

--- server/src/controllers/user.controllers.ts
+++ server/src/controllers/user.controllers.ts
@@ -7,6 +7,4 @@ export const perfil = async (req: IReq, res: IRes) => {
     const {email} = (req as CustomRequest).payload as Payload
     const found = await Usuario.findOne({email})
     res.status(200).json(found)
-
-    
 }
